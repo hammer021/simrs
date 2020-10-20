daf0f92ff8338ee838b3dc7e73a48f43edfebd58
Luqman <e41180965@student.polije.ac.id>
2020-10-20 19:05:24 +0700

    update regis

--- Telemedicine/app/src/main/java/com/example/telemedicine/Opening.java
+++ Telemedicine/app/src/main/java/com/example/telemedicine/Opening.java
@@ -2,13 +2,26 @@ package com.example.telemedicine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Opening extends AppCompatActivity {
+
+    Button regis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
+        regis = findViewById(R.id.btnregis);
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Opening.this, RegisterActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
