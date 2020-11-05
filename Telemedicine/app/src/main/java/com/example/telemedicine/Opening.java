e291188d5cd048231f5c8df4f9fdaad01fb564b7
Birrilfebrian <mubifaz12@gmail.com>
2020-11-05 15:29:00 +0700

    coba

--- Telemedicine/app/src/main/java/com/example/telemedicine/Opening.java
+++ Telemedicine/app/src/main/java/com/example/telemedicine/Opening.java
@@ -3,9 +3,12 @@ package com.example.telemedicine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Opening extends AppCompatActivity {
 
@@ -16,10 +19,18 @@ public class Opening extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
         regis = findViewById(R.id.btnregis);
+        TextView login = (TextView) findViewById(R.id.txtlogin);
         regis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent a = new Intent(Opening.this, RegisterActivity.class);
+                startActivity(a);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Opening.this, LoginActivity.class);
                 startActivity(a);
             }
         });
