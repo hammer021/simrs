5f06f10809225b22233b28195c84ac5e40c3b8f2
Luqman <e41180965@student.polije.ac.id>
2020-11-26 17:58:31 +0700

    Buat birril

--- Telekonsultasi/app/src/main/java/com/example/telekonsultasi/LoginActivity.java
+++ Telekonsultasi/app/src/main/java/com/example/telekonsultasi/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.telekonsultasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnlogin = findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(LoginActivity.this, NavFragment.class);
+                startActivity(a);
+                finish();
+            }
+        });
     }
 }
---
+++ Telekonsultasi/app/src/main/java/com/example/telekonsultasi/PasienTerdaftarActivity.java
@@ -0,0 +1,14 @@
+package com.example.telekonsultasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasienTerdaftarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasien_terdaftar);
+    }
+}
