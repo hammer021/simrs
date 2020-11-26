6227578a18415f32c85cf14b0f7231a255413749
eilham <e41180954@student.polije.ac.id>
2020-11-26 14:52:32 +0700

    login model and helper

---
+++ Web/application/models/Admin_model.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin_model extends CI_Model
+{
+    public function getUserRoleById($role_id)
+    {
+        return $this->db->get_where('tb_role', ['id' => $role_id])->row_array();
+    }
+    public function getUserRoleAll()
+    {
+        return $this->db->get('tb_role')->result_array();
+    }
+    public function searchUserData()
+    {
+        $keyword = $this->input->post('keyword', true);
+        $this->db->like('name', $keyword);
+        $this->db->or_like('email', $keyword);
+        $this->db->or_like('username', $keyword);
+        return $this->db->get('tb_registrasi')->result_array();
+    }
+}
