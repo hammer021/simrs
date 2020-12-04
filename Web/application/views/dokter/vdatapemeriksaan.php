5b82e6d8897e5e0858eacc56dc92fdedf4457492
AchmadIqbal <baidowi.jr.10@gmail.com>
2020-12-04 19:23:36 +0700

    Dr ( DataPemeriksaan)
    back endnya belum ya, masih sama kayak yg data dokter admin

--- Web/application/views/dokter/vdatapemeriksaan.php
+++ Web/application/views/dokter/vdatapemeriksaan.php
@@ -3,7 +3,7 @@
 		<div class="content-wrapper-before"></div>
 		<div class="content-header row">
 			<div class="content-header-left col-md-4 col-12 mb-2">
-				<h3 class="content-header-title" style="margin-bottom:50px;margin-top:80px">DATA DOKTER</h3>
+				<h3 class="content-header-title" style="margin-bottom:50px;margin-top:80px">Data Pemeriksaan</h3>
 			</div>
 			<div class="content-header-right col-md-8 col-12">
 				<div class="breadcrumbs-top float-md-right">
@@ -11,7 +11,7 @@
 						<ol class="breadcrumb">
 							<li class="breadcrumb-item"><a href="index.html">Home</a>
 							</li>
-							<li class="breadcrumb-item active">Data Pemeriksaan
+							<li class="breadcrumb-item active">
 							</li>
 						</ol>
 					</div>
@@ -37,7 +37,7 @@
 					<div class="card">
 						<div class="card-header">
 							<h4 class="card-title">
-								<p><span class="text-bold-600"><button class="btn btn-primary" data-toggle="modal" data-target="#tambah"><i class="fa fa-plus"></i> Tambah Data Dokter</button></span></p>
+								<p><span class="text-bold-600"><button class="btn btn-primary" data-toggle="modal" data-target="#tambah"><i class="fa fa-plus"></i> Tambah Pasien</button></span></p>
 							</h4>
 							<a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
 							<div class="heading-elements">
@@ -54,12 +54,13 @@
 								<table class="table">
 									<thead class="thead-dark" align="center">
 										<tr>
-											<th>No</th>
-											<th>No Praktek</th>
-											<th>Nama Dokter</th>
-											<th>Jadwal Praktek</th>
-											<th>Nomor HP Dokter</th>
-											<th>Foto</th>
+											<th>MR</th>
+											<th>Nama</th>
+											<th>Umur</th>
+											<th>JK</th>
+											<th>Alamat</th>
+											<th>Tanggal Lahir</th>
+											<th>Telepon</th>
 											<th>Aksi</th>
 										</tr>
 									</thead>
