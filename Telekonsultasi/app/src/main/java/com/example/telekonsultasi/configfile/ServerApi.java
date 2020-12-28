a46cfe8ea10c8a0a270891059c25b7b0870f8f82
ratnadwikristinasari <ratnadwikristinasari27@gmail.com>
2020-12-28 22:43:41 +0700

    dashboard android

--- Telekonsultasi/app/src/main/java/com/example/telekonsultasi/configfile/ServerApi.java
+++ Telekonsultasi/app/src/main/java/com/example/telekonsultasi/configfile/ServerApi.java
@@ -2,7 +2,8 @@ package com.example.telekonsultasi.configfile;
 
 public class ServerApi {
  //     public static final String IPServer="http://macarinaid.wsjti.com/RestApi/";
-      public static final String IPServer="http://192.168.0.20/simrs/Web/api/";
+     // public static final String IPServer="http://192.168.0.20/simrs/Web/api/";
+    public static final String IPServer="http://192.168.43.76/simrs/Web/api/";
 //      public static final String IPServer="http://192.168.43.254/simrs/Web/api/";
     public static final String URL_REGIS=IPServer+"Registerakun";
     public static final String URL_LOGIN=IPServer+"Api_login";
@@ -15,9 +16,10 @@ public class ServerApi {
     public static final String URL_GETPASIEN=IPServer+"Konsul/pasienselesai?kd_regist=";
     public static final String URL_BUKTI=IPServer+"Konsul/bukti";
     public static final String URL_RESEP=IPServer+"Konsul/selesai";
+    public static final String URL_DASHBOARD=IPServer+"Konsul/dashboard";
     public static final String URL_TAMPILCHAT=IPServer+"Api_chat";
     public static final String URL_UPLOAD_BUKTI=IPServer+"Konsul/bukti";
-    public static final String URL_PASFOTO="http://192.168.0.20/simrs/Web/assets/images/user/";
-    public static final String URL_FOTOPASIEN="http://192.168.0.20/simrs/Web/assets/images/pasien/";
+    public static final String URL_PASFOTO="http://192.168.43.76/simrs/Web/assets/images/user/";
+    public static final String URL_FOTOPASIEN="http://192.168.43.76/simrs/Web/assets/images/pasien/";
     public static final String URL_GAMBARBRG="http://macarinaid.wsjti.com/uploads/barang/";
 }
--- Telekonsultasi/app/src/main/java/com/example/telekonsultasi/ui/home/HomeFragment.java
+++ Telekonsultasi/app/src/main/java/com/example/telekonsultasi/ui/home/HomeFragment.java
@@ -12,13 +12,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.telekonsultasi.NotificationActivity;
 import com.example.telekonsultasi.R;
+import com.example.telekonsultasi.configfile.ServerApi;
 import com.example.telekonsultasi.ui.periksa.PeriksaFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.jar.JarException;
 
 public class HomeFragment extends Fragment {
     ImageView notif;
+    TextView pasiendas, keluhandas, obatdas;
     ConstraintLayout pasien, keluhan, obat;
     @Nullable
     @Override
@@ -49,6 +65,46 @@ public class HomeFragment extends Fragment {
                 startActivity(d);
             }
         });
+
+        obatdas = v.findViewById(R.id.jmlobat);
+        keluhandas = v.findViewById(R.id.jmlkeluhan);
+        pasiendas = v.findViewById(R.id.jmlpasien);
+        hitung_dashboard();
         return v;
+
+    }
+    public void hitung_dashboard() {
+        final StringRequest jumlah = new StringRequest(Request.Method.GET, ServerApi.URL_DASHBOARD, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject a = new JSONObject(response);
+                    boolean status = a.getBoolean("status");
+                    if (status) {
+                        JSONArray data = a.getJSONArray("data");
+                        JSONObject param = data.getJSONObject(0);
+
+                        String r = param.getString("jumlah_pasien");
+                        String d = param.getString("jumlah_periksa_1");
+                        String k = param.getString("jumlah_obat_1");
+
+                        pasiendas.setText(r);
+                        keluhandas.setText(d);
+                        obatdas.setText(k);
+                    } else {
+                        Toast.makeText(getActivity(), "Tidak Ada Data", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+                }
+        });
+        RequestQueue req = Volley.newRequestQueue(getContext());
+        req.add(jumlah);
     }
 }
