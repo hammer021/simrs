05e800df24e862e0fe218b74df34876fc5303aa1
eilham <e41180954@student.polije.ac.id>
2020-12-30 02:38:18 +0700

    update dan hapus admin

--- Web/application/models/Admin_model.php
+++ Web/application/models/Admin_model.php
@@ -19,7 +19,34 @@ class Admin_model extends CI_Model
         $this->db->or_like('username', $keyword);
         return $this->db->get('tb_registrasi')->result_array();
     }
+    public function tampildata()
+    {
+        return $this->db->query(" SELECT tb_registrasi.kd_regist, tb_registrasi.name, 
+		tb_registrasi.email, tb_registrasi.alamat, tb_registrasi.no_hp, 
+		tb_registrasi.tgl_lahir, tb_registrasi.tempat_lahir, tb_registrasi.image 
+        FROM tb_registrasi WHERE tb_registrasi.kd_role = 1")->result();
 
+    }
+    function update_data($where,$data,$table){
+		$this->db->where($where);
+		$this->db->update($table,$data);
+  }	
+  function hapus_data($id){
+    $this->_deleteImage($id);
+    return $this->db->delete($this->_table, array("kd_regist" => $id));
+  }
+  public function getById($id)
+  {
+      return $this->db->get_where($this->_table, ["kd_regist" => $id])->row();
+  }
+private function _deleteImage($id)
+{
+$data = $this->getById($id);
+if ($data->image != "default.jpg") {
+    $filename = explode(".", $data->image)[0];
+    return array_map('unlink', glob(FCPATH."assets/images/admin/$filename.*"));
+}
+}
     public function buat_kode()
     {
         $this->db->select('RIGHT(tb_registrasi.kd_regist,4) as kode', FALSE);
